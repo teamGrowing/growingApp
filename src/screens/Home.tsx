de34ba031f2ad135aba447a752c7f9b5fd4e4906
Yeonju Jo <yjj0287@gmail.com>
2023-03-26 03:21:54 +0900

    feat: modify webview

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,51 +1,38 @@
 import React, { useRef } from 'react';
 import WebView from 'react-native-webview';
 import {
-  RefreshControl,
-  SafeAreaView,
-  ScrollView,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  TouchableWithoutFeedback,
   StyleSheet,
 } from 'react-native';
 
-function wait(timeout: number) {
-  return new Promise<void>((resolve) => {
-    setTimeout(resolve, timeout);
-  });
-}
-
 const Home = () => {
-  const [refreshing, setRefreshing] = React.useState(false);
   const webViewRef = useRef<WebView | null>(null);
 
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    webViewRef.current?.reload();
-    wait(2000).then(() => setRefreshing(false));
-  }, [refreshing]);
-
   return (
-    <>
-      <SafeAreaView style={{ flex: 1, backgroundColor: 'white' }}>
-        <ScrollView
-          style={styles.ScrollStyle}
-          contentContainerStyle={{ flex: 1 }}
-          refreshControl={
-            <RefreshControl refreshing={false} onRefresh={onRefresh} />
-          }
-        >
-          <WebView
-            ref={webViewRef}
-            automaticallyAdjustContentInsets={false}
-            source={{ uri: 'http://localhost:3000' }}
-            allowsFullscreenVideo={true}
-            javaScriptEnabled={true}
-            domStorageEnabled={true}
-            startInLoadingState={true}
-            style={styles.container}
-          />
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={styles.container}
+    >
+      <TouchableWithoutFeedback
+        onPress={Keyboard.dismiss}
+        style={styles.container}
+      >
+        <WebView
+          ref={webViewRef}
+          source={{ uri: 'http://localhost:3000' }}
+          allowsFullscreenVideo={false}
+          javaScriptEnabled={true}
+          domStorageEnabled={true}
+          startInLoadingState={true}
+          hideKeyboardAccessoryView={true}
+          scrollEnabled={false}
+          style={styles.container}
+        />
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -53,10 +40,7 @@ export default Home;
 
 const styles = StyleSheet.create({
   container: {
-    position: 'relative',
-  },
-  ScrollStyle: {
+    flex: 1,
     backgroundColor: 'white',
-    position: 'relative',
   },
 });
